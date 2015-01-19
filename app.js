7879c0faa5a17f44d3734d82b3c28379638e5008
David Rivers <dmr41.rivers@gmail.com>
2015-01-19 09:59:43 -0700

    full functioning app - libraries and songs not included in push.

--- app.js
+++ app.js
@@ -1,35 +1,32 @@
 $("document").ready(function (){
-  $.getJSON("data.json", function(songs) {
+var hmm =  $.getJSON("data.json", function(songs) {
     var songTemplate = $("#songTemplate").html();
     var html_out = Mustache.to_html(songTemplate, songs);
      $("#music-player").html(html_out);
-  });
+     datas = function(){
+        return songs;
+     };
 
-  $('#music-player').on('click', '.fa-play' ,function() {
+  $('#music-player').on('click', '.fa-play' , function() {
+    var hi = datas();
     var n = $(this).closest('article div h2').attr("id") - 1;
+    var songString = "tracks/" + hi.tracks[n].file + ".mp3"
+    $('audio').attr('src', '');
+    $(this).siblings('audio').attr('src', songString);
     var head = $(this).closest('article div h2').data("songtitle");
-    console.log(head);
     head = "Now Playing: " + head;
-    $('audio').each(function(){
-      this.pause();
-    });
-
     $('audio')[n].play();
-      $(this).addClass("fa-pause").removeClass("fa-play");
-      $('.fa').not(this).removeClass("fa-pause").addClass("fa-play");
-
+    $(this).addClass("fa-stop").removeClass("fa-play");
+    $('.fa').not(this).removeClass("fa-stop").addClass("fa-play");
     $('#head-grab h2').addClass("play-header").removeClass('noplay-header');
     $('.play-header').text(head);
     });
-
-  $('#music-player').on('click', '.fa-pause', function() {
-    var n = $(this).closest('article div h2').attr("id") - 1;
-    $('article div audio')[n].pause();
-    $(this).addClass("fa-play").removeClass("fa-pause");
-
-    $('#head-grab h2').removeClass("play-header").addClass('noplay-header');
-    $('.noplay-header').text('Select a song!');
-
   });
 
+  $('#music-player').on('click', '.fa-stop', function() {
+    $(this).siblings('audio').attr('src', '')
+    $(this).addClass("fa-play").removeClass("fa-stop");
+    $('#head-grab h2').removeClass("play-header").addClass('noplay-header');
+    $('.noplay-header').text('Select a song!');
+  });
 });
